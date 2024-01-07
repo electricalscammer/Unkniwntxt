0a2739a96f88f6747c142507bc7cbac04ffc9d9d
electricalscammer <155905329+electricalscammer@users.noreply.github.com>
2024-01-07 22:48:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6698282518:AAEMINBsffaF8x5rkFtf5y60Dv-YGjI-NJw")
-    API_ID = int(os.environ["API_ID", 21104697]
-    API_HASH = os.environ["API_HASH", "2bbb3dc6c94bac3e5fcc492e86475063"]
-    AUTH_USERS = "5003073626 """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5881186368:AAE9NQzCRpKz8HA0uWaLCe-MimjSGrkvcWw")
+    API_ID = int(os.environ["API_ID", 24262622]
+    API_HASH = os.environ["API_HASH", "50831eb3329ed9c0557aa2bc6aa34376"]
+    AUTH_USERS = "1477222048 """
 
